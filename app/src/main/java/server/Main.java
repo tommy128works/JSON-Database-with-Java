38e8d9853d141f381836263ffba03711c9571f1c
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-07-07 22:36:35 -0400

    Set up server.Main to run before client.Main in one gradle task

---
+++ app/src/main/java/server/Main.java
@@ -0,0 +1,12 @@
+package server;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Server!");
+
+
+//        for investigating work directory
+//        String path = System.getProperty("user.dir") + "/src/client/data/";
+//        System.out.println(path);
+    }
+}
